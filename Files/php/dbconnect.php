3f8727ed6807f91d1a274846b9e451b947b67bea
aktyis <istyak370@gmail.com>
2015-08-07 03:03:11 +0600

    log in success

--- Files/php/dbconnect.php
+++ Files/php/dbconnect.php
@@ -1,12 +1,11 @@
 <?php
-$db_conx = mysqli_connect("localhost", "root", "", "webtechprojectdatabase");
-// Evaluate the connection
+$db_connect = mysqli_connect("localhost" , "root" , "" ,"webtechprojectdatabase");
+
 if (mysqli_connect_errno()) 
 {
     echo mysqli_connect_error();
     exit();
-} else 
-{
-	echo "Success";
-}
+} 
+else 
+{	echo "Success";	}
 ?>
--- Files/php/file.php
+++ Files/php/file.php
@@ -1,32 +1,20 @@
 <?php
-
-include("dbconnect.php");
+require './dbconnect.php';
+require './check.php';
 
 session_start();
 
 $user_ok = false;
+
 $log_username = "";
 $log_password = "";
-// User Verify function
 
-
-function evalLoggedUser($conx,$u,$p)
-{
-	$sql = "SELECT * FROM users WHERE username='$u'AND password='$p';
-    $query = mysqli_query($conx, $sql);
-    $numrows = mysqli_num_rows($query);
-	if($numrows > 0)
-    {
-		return true;
-	}
-}
-
-if( (isset($_SESSION["user"]) && (isset( $_SESSION["pass"]))
+if ((isset($_SESSION["user"]) ) && (isset( $_SESSION["pass"])))
 {
 	$log_username = preg_replace('#[^a-z0-9]#i', '', $_SESSION['user']);
 	$log_password = preg_replace('#[^a-z0-9]#i', '', $_SESSION['pass']);
 	// Verify the user
-	$user_ok = evalLoggedUser($db_conx,$log_username,$log_password);
+	$user_ok = evalLoggedUser($db_connnect,$log_username,$log_password);
 }
 else if ( (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"]) ) )
 {
@@ -38,16 +26,15 @@ else if ( (isset($_COOKIE["user"])) && (isset($_COOKIE["pass"]) ) )
 	$log_password = $_SESSION['pass'];
     
 	// Verify the user
-	$user_ok = evalLoggedUser($db_conx,$log_username,$log_password);
+	$user_ok = evalLoggedUser($db_connnect,$log_username,$log_password);
     
 }
-
 // If user is already logged in, redirect
 
 
 if($user_ok == true)
 {
-	header("location: user.php?u=".$_SESSION["user"]); // redirect to appropieat user
+	header("location: ?u=".$_SESSION["user"]); // redirect to appropieat user
     exit();
 }
 
@@ -56,7 +43,7 @@ if(isset($_POST["username"]))
 {
 
 	// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
-	$username = $_POST['username']);
+	$username = $_POST['username'];
 	$password = md5($_POST['password']);
     
 	// FORM DATA ERROR HANDLING
@@ -67,12 +54,13 @@ if(isset($_POST["username"]))
 	} 
     else 
     {
+        $active="active";
 	   // END FORM DATA ERROR HANDLING
-		$sql = "SELECT * FROM users WHERE username='username' AND validity='active'";
-        $query = mysqli_query($db_conx, $sql);
+		$sql = "SELECT * FROM users WHERE user_id='$username' AND validity='$active'";
+        $query = mysqli_query($db_connnect, $sql);
         $row = mysqli_fetch_row($query);
         
-		$db_username = $row[username];
+		$db_username = $row[user_id];
         $db_pass_str = $row[password];
         
 		if($password != $db_pass_str)
