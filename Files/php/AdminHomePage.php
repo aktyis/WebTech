dd92c52fb9099ee08f280364ca83dfb3f90f0bd5
Nayeem Islam <nayeem0099@live.com>
2015-08-08 15:15:47 +0600

    admin home page static

---
+++ Files/php/AdminHomePage.php
@@ -0,0 +1,62 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>Employee</title>
+		<link rel="stylesheet" href="../css/homeStyle.css">
+	</head>
+	<body >
+            <div align="center" class="div1">
+                <div align="center" class="div2">
+                    <div align="left" class="div3"><br>
+                        <form action="" method="get">
+                            <label id="adminHomeLogo">E U B</label>
+                        <a class="alink" id="hlink" href="AdminHomePage.php" style="margin-left: 30em;">Home</a>
+                        <a class="alink" id="falink" href="">Profile</a>
+                        <a class="alink" id="stlink" href="">Change Password</a>
+                        <a class="alink" id="adlink" href="logout.php">Logout</a>
+                        </form>
+                    </div><br><br>
+                </div>
+        </div>
+        <div align="center">
+            <div align="center" style="width:1100px; height:800px;" >
+            <table align="left" style="width:1100px; height:800px;">
+                <tr>
+                    <td style="width:180px; border:1px; vertical-align: top; text-align: left;">
+                        <label id="academic">Academic</label><br>
+                        <a class="adlink" id="calink" href="">Courses</a><br><br>
+                        <a class="adlink" id="cstlink" href="">Class Schedule</a><br><br>
+                        <a class="adlink" id="relink" href="">Registration</a><br><br>
+                        <label id="academic">Library</label><br>
+                        <a class="adlink" id="sblink" href="">Search Book</a><br><br>
+                        <a class="adlink" id="cbtlink" href="">Current Borrows</a><br><br>
+                        <a class="adlink" id="bhlink" href="">Borrow History</a><br><br>
+                        <label id="academic">Administrative</label><br>
+                        <a class="adlink" id="aslink" href="">Add Student</a><br><br>
+                        
+                    </td>
+                    <td style="width:700px; border:1px solid green; vertical-align: top; text-align: left; margin-left: 2em;">
+                        <label id="academic">Academic</label><br>
+                        <a class="adlink" id="calink" href="">Courses</a><br><br>
+                        <a class="adlink" id="cstlink" href="">Class Schedule</a><br><br>
+                        <a class="adlink" id="relink" href="">Registration</a><br><br>
+                        <label id="academic">Library</label><br>
+                        <a class="adlink" id="calink" href="">Courses</a><br><br>
+                        <a class="adlink" id="cstlink" href="">Class Schedule</a><br><br>
+                        <a class="adlink" id="relink" href="">Registration</a><br><br>
+                    </td>
+                    <td style="width:200px; border:1px; vertical-align: top; text-align: center;">
+                        <label id="academic"></label><br>
+                        <a class="adlink" id="calink" href="">Name <br> Id</a><br><br>
+                        
+                    </td>
+                </tr>
+            </table>
+            </div>
+        </div>
+        
+        <?php
+              
+        ?>
+	</body>
+</html>
